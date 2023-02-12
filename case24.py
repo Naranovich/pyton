c05044b0cd051b02a50fea5cab5bf668b0cc3a75
Naranovich <nik.naranovich@mail.ru>
2023-02-13 00:15:18 +0300

    case28

--- case24.py
+++ case24.py
@@ -2,7 +2,7 @@ n = int( input( 'Введите число кустов ' ) )
 lis = [ int(x) for x in input( 'Введите количество ягод на кусте ' ).split() ]
 n = len(lis)
 lis = lis + lis[:2]
-ma = 0
+m = 0
 for i in range(n):
-    ma = max( ma, lis[i] + lis[i+1] + lis[i+2] )
-print(ma)
+    m = max( m, lis[i] + lis[i+1] + lis[i+2] )
+print(m)
