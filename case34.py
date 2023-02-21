4b43272033c641ec461053a953bc93ceefe7f19e
Naranovich <nik.naranovich@mail.ru>
2023-02-21 22:11:02 +0300

    case34

---
+++ case34.py
@@ -0,0 +1,8 @@
+a = input('print :')
+st = a.lower().split()
+f = lambda x: sum(1 for i in x if i in 'аеёиоуыэюя')
+t = f(st[0])
+if all([f(i) == t for i in st]):
+    print('Парам пам-пам')
+else:
+    print('Пам парам')
